5da4ca21f035a52c63cd0c2011600df3db51b4b3
Wektors <wiktorm16@gmail.com>
2023-01-25 22:02:15 +0100

    ledger and deposit

--- budget.py
+++ budget.py
@@ -1,7 +1,14 @@
 class Category:
+    def __init__(self):
+        self.ledger = []
+    def deposit(self, amount, description = ""):
+        self.ledger.append({"amount": amount, "description": description})
 
-
-
+budget = Category()
+budget.deposit(50, "apples")
+print(budget.ledger)
 
 
 def create_spend_chart(categories):
+    none = 0
+
