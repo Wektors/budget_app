f59e1635d3e002f1893c47e197b6d2eba05d27fa
Wektors <wiktorm16@gmail.com>
2023-02-19 18:50:12 +0100

    methods ready

--- budget.py
+++ budget.py
@@ -3,16 +3,22 @@ class Category:
         self.name = name
         self.ledger = []
 
+    def check_funds(self, amount):
+        curr_amount = 0
+        for each in self.ledger:
+            curr_amount += each["amount"]
+        if curr_amount >= amount:
+            return True
+        else:
+            return False
+
     def deposit(self, amount, description=""):
         self.ledger.append({"amount": amount, "description": description})
 
     def withdraw(self, amount, description=""):
-        curr_amount = 0
-        for each in self.ledger:
-            curr_amount += each["amount"]
 
         negative_amount = amount * -1
-        if curr_amount >= amount:
+        if self.check_funds(negative_amount):
             self.ledger.append(
                 {"amount": negative_amount, "description": description})
             return True
@@ -24,10 +30,32 @@ class Category:
         for each in self.ledger:
             sum += each["amount"]
         return sum
-    
+
     def transfer(self, amount, another_cat):
-        self.withdraw(amount, "Transfer from " + self.name)
-        another_cat.deposit(amount, "Transfer to " + self.name)
+
+        if self.check_funds(amount):
+            self.withdraw(amount, "Transfer from " + self.name)
+            another_cat.deposit(amount, "Transfer to " + self.name)
+            return True
+
+        else:
+            return False
+
+    def __str__(self):
+        asts = []  # asterisks
+        for each in range(round((29 - len(self.name)) / 2)):
+            asts.append("*")
+
+        if len(self.name) % 2 == 0:
+            extra_ast = ""
+        else:
+            extra_ast = "*"
+
+        title_line = ''.join(asts) + extra_ast + \
+            self.name + ''.join(asts) + "\n"
+
+        return title_line + 
+
 
 budget = Category("budget")
 
@@ -37,11 +65,12 @@ savings = Category("savings")
 
 savings.deposit(50, "initialfunds")
 
-budget.transfer(30, savings)
+budget.transfer(25, savings)
 
-print(budget.get_balance())
 
-print(savings.get_balance())
+print(savings)
+
+print(budget)
 
 
 def create_spend_chart(categories):
